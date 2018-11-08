e8d5904da4ceb056a3f8f77625c8197af82e3ab6
Tyrese <tyrese.hughey.2021@student.lhschools.org>
2018-11-08 08:49:05 -0500

    Pulled project under source control

---
+++ src/Hangman.java
@@ -0,0 +1,48 @@
+public class Hangman {
+
+    public static void main(String[] args){
+
+    System.out.println("Welcome to Hangman!");
+    System.out.println("Rules of the game: Guess a letter for the amount of spaces given. ");
+    System.out.println("For every letter you get wrong, a part of the hangman will appear.");
+    System.out.println("You have six chances before the whole man is there and once that happens YOU LOSE!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
