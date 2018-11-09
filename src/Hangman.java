73008e207241fa1eaecf973ca7cd138ecd523248
Tyrese <tyrese.hughey.2021@student.lhschools.org>
2018-11-09 08:12:03 -0500

    Started to make the hangman

--- src/Hangman.java
+++ src/Hangman.java
@@ -5,8 +5,21 @@ public class Hangman {
     System.out.println("Welcome to Hangman!");
     System.out.println("Rules of the game: Guess a letter for the amount of spaces given. ");
     System.out.println("For every letter you get wrong, a part of the hangman will appear.");
-    System.out.println("You have six chances before the whole man is there and once that happens YOU LOSE!");
+    System.out.println("Once the whole hangman appears you lose the game.");
 
+    int numberOfMisses=0;
+    for( int i=0; i<15; i++){
+        hangManImage(i);}
+    }
+public static void hangManImage(int tries){
+        String display="";
+        switch(tries){
+
+            case 0:
+            default:
+                System.out.println("    |---------------------------------|");
+                System.out.println("    |        ;")
+        }
 
 
 
